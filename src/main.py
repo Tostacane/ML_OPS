b94743a37d92176d439048bfd71cb4acc9d7f217
Davide <davide.villa@student.supsi.ch>
2026-03-31 09:15:02 +0200

    ADD: Added a name for the model file package to save.
    TODO: add the remaining parameters in the streamlit app to truly perform a predicion

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from ml_flow.mlflow import run_experiment
 
 
 if __name__ == "__main__":
-    model, X_test, y_test, y_pred, y_proba = train("hr_attrition")
-    metrics = evaluate(y_test, y_pred, y_proba)
+    model, X_test, y_test, y_pred, y_proba = train(dataset_name = "hr_attrition", save_file_name = "hr_log_reg")
+    # metrics = evaluate(y_test, y_pred, y_proba)
 
-    run_experiment()
+    # run_experiment()
--- streamlit/app.py
+++ streamlit/app.py
@@ -10,7 +10,7 @@ import streamlit as st
 from src.models.persistence import load_model
 
 
-MODEL = load_model("model.pkl")
+MODEL = load_model("hr_log_reg.pkl")
 
 st.set_page_config(
     page_title="Employee Attrition Predictor",
@@ -21,6 +21,20 @@ st.title("Employee Attrition Prediction App")
 st.write("Enter employee information to estimate the probability of attrition.")
 
 st.subheader("Employee Data")
+
+# TODO: add these parameters with streamlit and add also the bounds
+
+"""
+    ['Age', 'BusinessTravel', 'DailyRate', 'Department', 'DistanceFromHome',
+    'Education', 'EducationField', 'EnvironmentSatisfaction', 'Gender',
+    'HourlyRate', 'JobInvolvement', 'JobLevel', 'JobRole',
+    'JobSatisfaction', 'MaritalStatus', 'MonthlyIncome', 'MonthlyRate',
+    'NumCompaniesWorked', 'OverTime', 'PercentSalaryHike',
+    'PerformanceRating', 'RelationshipSatisfaction', 'StockOptionLevel',
+    'TotalWorkingYears', 'TrainingTimesLastYear', 'WorkLifeBalance',
+    'YearsAtCompany', 'YearsInCurrentRole', 'YearsSinceLastPromotion',
+    'YearsWithCurrManager']
+"""
 
 age = st.number_input("Age", min_value=0, max_value=100, value=0)
 income = st.number_input("Income", min_value=1000, max_value=50000, value=1000)

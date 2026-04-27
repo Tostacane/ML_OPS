fcc477482d5964310fd75af04943b6ffa99841cf
JulianCummaudo <julian.cummaudo@gmail.com>
2026-04-27 20:58:14 +0200

    final from me

--- run.sh
+++ run.sh
@@ -11,6 +11,6 @@ fi
 
 echo "Opening ML_Flow, FastAPI and Streamlit in separate tabs"
 
-konsole -p tabtitle="MLflow" -e bash -c "mlflow ui --port 5000; exec bash" &
+konsole -p tabtitle="MLflow" -e bash -c "mlflow ui --backend-store-uri sqlite:///ml_flow/mlflow.db --port 5000; exec bash" &
 konsole -p tabtitle="FastAPI" -e bash -c "uvicorn deploy.api:app --host 0.0.0.0 --port 8000; exec bash" &
 konsole -p tabtitle="Streamlit" -e bash -c "streamlit run streamlit/app.py; exec bash" &
